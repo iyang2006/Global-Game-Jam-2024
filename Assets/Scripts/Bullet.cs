a34bc2b370940136e0e7b9e4f886f6deade7a234
iyang2006 <yang2006@purdue.edu>
2024-01-27 13:37:43 -0500

    kb cat

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float bulletSpeed = 1;
     Vector3 bulletVector;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get bullet vector
-        GameObject player = GameObject.FindGameObjectsWithTag("player")[0];
-        float playerX = player.transform.position.x;
-        float playerY = player.transform.position.y;
-        float playerZ = player.transform.position.z;
-        float bulletX = transform.position.x;
-        float bulletY = transform.position.y;
-        float bulletZ = transform.position.z;
-        bulletVector = new Vector3(playerX - bulletX, playerY - bulletY, playerZ - bulletZ);
-        bulletVector.Normalize();
-        bulletVector *= bulletSpeed;
+
     }
 
     // Update is called once per frame
@@ -29,17 +18,21 @@ public class Bullet : MonoBehaviour
         transform.position += bulletVector * Time.deltaTime;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(collision.gameObject.tag);
-        if (collision.gameObject.tag == "world")
+        if (other.gameObject.tag == "world")
         {
             Destroy(gameObject);
         }
-        if (collision.gameObject.tag == "player")
+        if (other.gameObject.tag == "player")
         {
             // do damage to player
             Destroy(gameObject);
         }
+    }
+
+    public void GetBulletVector(Vector3 bulletVector)
+    {
+        this.bulletVector = bulletVector;
     }
 }
--- Assets/Scripts/KeyboardCat.cs
+++ Assets/Scripts/KeyboardCat.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class KeyboardCat : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
+    [SerializeField] float waitTime;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find player
+        player = GameObject.FindGameObjectWithTag("player");
+
+        StartCoroutine(waiting());
     }
 
     // Update is called once per frame
@@ -19,18 +27,30 @@ public class KeyboardCat : MonoBehaviour
     }
 
     // Shoots player
-    void Shoot()
+    void Shoot(Vector3 bulletVector, RaycastHit hit)
     {
-        // Get position of player
-        float playerX = player.transform.position.x;
-        float playerY = player.transform.position.y;
-        float playerZ = player.transform.position.z;
-
-
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(hit.normal, Vector3.forward));
+        bullet.GetComponent<Bullet>().GetBulletVector(bulletVector);
     }
 
-    void CreateBullet()
+    IEnumerator waiting()
     {
+        while (true)
+        {
+            // Get bullet speed
+            Vector3 bulletVector = player.transform.position - transform.position;
+            bulletVector.Normalize();
+            bulletVector *= bulletSpeed;
 
+            // Shoot player if cat sees player
+            if (Physics.Raycast(transform.position, bulletVector, out RaycastHit hit, 128, layerMask))
+            {
+                if (hit.transform.gameObject.tag == "player")
+                {
+                    Shoot(bulletVector, hit);
+                }
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 }
