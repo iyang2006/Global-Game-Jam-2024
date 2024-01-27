20efa12d2f568e056d20a444426694067f757b2d
iyang2006 <yang2006@purdue.edu>
2024-01-26 23:31:37 -0500

    bullet working

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -11,14 +11,14 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         // Get bullet vector
-        GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+        GameObject player = GameObject.FindGameObjectsWithTag("player")[0];
         float playerX = player.transform.position.x;
         float playerY = player.transform.position.y;
         float playerZ = player.transform.position.z;
         float bulletX = transform.position.x;
         float bulletY = transform.position.y;
         float bulletZ = transform.position.z;
-        Vector3 bulletVector = new Vector3(bulletX - playerX, bulletY - playerY, bulletZ - playerZ);
+        bulletVector = new Vector3(playerX - bulletX, playerY - bulletY, playerZ - bulletZ);
         bulletVector.Normalize();
         bulletVector *= bulletSpeed;
     }
@@ -26,11 +26,20 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += bulletVector;
+        transform.position += bulletVector * Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "world")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "player")
+        {
+            // do damage to player
+            Destroy(gameObject);
+        }
     }
 }
