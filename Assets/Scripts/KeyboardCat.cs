ac982b7025ccac18ce3230f0a0b322454eb54950
iyang2006 <yang2006@purdue.edu>
2024-01-26 21:05:06 -0500

    m

---
+++ Assets/Scripts/KeyboardCat.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardCat : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Shoots player
+    void Shoot()
+    {
+        // Get position of player
+        float playerX = player.transform.position.x;
+        float playerY = player.transform.position.y;
+        float playerZ = player.transform.position.z;
+
+
+    }
+
+    void CreateBullet()
+    {
+
+    }
+}
