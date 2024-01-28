d7f448b956ada7689588bd672d8b13e248fb54ca
apboezer <aboezer@purdue.edu>
2024-01-28 13:32:24 -0500

    added smoke effect to weapons

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -20,6 +20,7 @@ public class WeaponScript : MonoBehaviour
     [SerializeField] private PlayerAudio audioControl;
     [SerializeField] private SniperAudio leftSniperAudio;
     [SerializeField] private SniperAudio rightSniperAudio;
+    [SerializeField] private GameObject smokePrefab;
 
     
     private int ammoL;
@@ -37,11 +38,15 @@ public class WeaponScript : MonoBehaviour
     private Quaternion prevQuat;
     private float lastSpinTrue;
 
+    [SerializeField] private Transform leftSmokeStart;
+    [SerializeField] private Transform rightSmokeStart;
+
     public void SetTurningLeft(bool val) {
         turningLeft = val;
     }
 
     private void FireLeft() {
+        Debug.Log("firing left");
         if (ammoL <= 0) {
             leftSniperAudio.PlayEmpty();
         }
@@ -55,13 +60,14 @@ public class WeaponScript : MonoBehaviour
                 leftSniperAudio.PlayShot();
             }
             controller.ShootLeft();
-            Fire();
+            Fire(true);
         }
         controller.SetDull();
         hasSpun = false;
     }
 
     private void FireRight() {
+        Debug.Log("firing right");
         if (ammoR <= 0) {
             rightSniperAudio.PlayEmpty();
         }
@@ -75,15 +81,23 @@ public class WeaponScript : MonoBehaviour
                 rightSniperAudio.PlayShot();
             }
             controller.ShootRight();
-            Fire();
+            Fire(false);
         }
         controller.SetDull();
         hasSpun = false;
     }
 
-    private void Fire() {
+    private void Fire(bool isLeft) {
         RaycastHit hit;
+        Vector3 startPos;
+        if (isLeft) {
+            startPos = leftSmokeStart.position;
+        }
+        else {
+            startPos = rightSmokeStart.position;
+        }
         if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, maxRange, playerMask)) {
+            StartCoroutine(SpawnSmoke(startPos, hit.point));
             if (hit.collider.gameObject.tag == "enemy") {
                 float tempDmg = damage;
                 if (hasSpun) {
@@ -96,6 +110,9 @@ public class WeaponScript : MonoBehaviour
                 Health healthScript = hit.collider.gameObject.GetComponentInParent<Health>();
                 healthScript.damageEntity(tempDmg);
             }
+        }
+        else {
+            StartCoroutine(SpawnSmoke(startPos, startPos + (cameraTransform.forward.normalized * maxRange)));
         }
     }
 
@@ -120,12 +137,10 @@ public class WeaponScript : MonoBehaviour
         }
         hasSpun = true;
         lastSpinTrue = Time.time;
-        Debug.Log("has spun");
     }
 
     private void UpdateSpin() {
         if (hasSpun && (Time.time - lastSpinTrue > spinShootTimeLimit)) {
-            Debug.Log("spin time out");
             audioControl.PlayPowerDown();
             controller.SetDull();
             hasSpun = false;
@@ -191,6 +206,18 @@ public class WeaponScript : MonoBehaviour
             }
         }
 
+    }
+
+    IEnumerator SpawnSmoke(Vector3 startPos, Vector3 endPos) {
+        //Debug.Log("spawnSmoke");
+        
+        Vector3 smokeVector = endPos - startPos;
+
+        GameObject smokeObj = Instantiate(smokePrefab, smokeVector / 2 + startPos, Quaternion.LookRotation(smokeVector, Vector3.up));
+        smokeObj.transform.Rotate(Vector3.right, 90, Space.Self);
+        smokeObj.transform.localScale = new Vector3(smokeObj.transform.localScale.x, smokeVector.magnitude / 2, smokeObj.transform.localScale.z);
+
+        yield break;
     }
 
 }
