d998809105240f576bc0f0905b4a696099e3bcd1
iyang2006 <yang2006@purdue.edu>
2024-01-27 20:57:32 -0500

    laser comp

--- Assets/Scripts/ShoopDaWhoop.cs
+++ Assets/Scripts/ShoopDaWhoop.cs
@@ -23,19 +23,14 @@ public class ShoopDaWhoop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerVector = player.transform.position - transform.position;
-
-        if (Physics.Raycast(transform.position, playerVector, out RaycastHit hit, 256, layerMask))
-        {
-            Debug.DrawRay(transform.position, hit.transform.position);
-        }
+        
     }
 
     IEnumerator waiting()
     {
-        //while (true)
+        while (true)
         {
-            /*
+            
             // play sound queue
 
             yield return new WaitForSeconds(aimTime);
@@ -48,34 +43,9 @@ public class ShoopDaWhoop : MonoBehaviour
             // Deploy laser
             if (Physics.Raycast(transform.position, playerVector, out RaycastHit hit, 256, layerMask))
             {
-                Vector3 laserVector = hit.transform.position - transform.position;
+                Vector3 laserVector = hit.point - transform.position;
                 Debug.DrawRay(transform.position, laserVector, Color.red);
 
-                GameObject laser = Instantiate(laserPrefab, playerVector / 2 + transform.position,
-                            Quaternion.LookRotation(laserVector, Vector3.up));
-                laser.transform.Rotate(Vector3.right, 90, Space.Self);
-                laser.transform.localScale = new Vector3(1, laserVector.magnitude / 2, 1);
-
-                yield return new WaitForSeconds(laserTime);
-
-                Destroy(laser);
-            }
-
-
-
-            // Play sound queue and fire laser
-            if (Physics.Raycast(transform.position, playerVector, out RaycastHit hit, 128, layerMask))
-            {
-                // play sound queue
-
-                yield return new WaitForSeconds(aimTime);
-
-                playerVector = player.transform.position - transform.position;
-                Vector3 laserVector = hit.transform.position - transform.position;
-
-                yield return new WaitForSeconds(0.4f);
-
-                // Deploy laser
                 GameObject laser = Instantiate(laserPrefab, laserVector / 2 + transform.position,
                             Quaternion.LookRotation(laserVector, Vector3.up));
                 laser.transform.Rotate(Vector3.right, 90, Space.Self);
@@ -84,12 +54,7 @@ public class ShoopDaWhoop : MonoBehaviour
                 yield return new WaitForSeconds(laserTime);
 
                 Destroy(laser);
-            }*/
-
-
-            // play sound queue
-
-            
+            }
         }
     }
 }
