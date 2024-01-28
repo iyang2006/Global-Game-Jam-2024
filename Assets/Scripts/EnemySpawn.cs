ae0433a2a250a2cca7534dcb5dca619545fbc678
iyang2006 <yang2006@purdue.edu>
2024-01-28 00:27:57 -0500

    a

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public static int level = 1;
+    [SerializeField] float levelSize = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPepe()
+    {
+
+    }
+
+    void SpawnKeyboardCat()
+    {
+
+    }
+
+    void SpawnTroll()
+    {
+
+    }
+
+    void Spawn
+}
