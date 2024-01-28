bc7a1b87dba6f62169e91fb33160fd4fad90987e
iyang2006 <yang2006@purdue.edu>
2024-01-27 20:25:05 -0500

    n

--- Assets/Scripts/ShoopDaWhoop.cs
+++ Assets/Scripts/ShoopDaWhoop.cs
@@ -30,6 +30,7 @@ public class ShoopDaWhoop : MonoBehaviour
     {
         while (true)
         {
+            /*
             // play sound queue
 
             yield return new WaitForSeconds(aimTime);
@@ -53,6 +54,41 @@ public class ShoopDaWhoop : MonoBehaviour
                 yield return new WaitForSeconds(laserTime);
 
                 Destroy(laser);
+            }
+
+
+
+            // Play sound queue and fire laser
+            if (Physics.Raycast(transform.position, playerVector, out RaycastHit hit, 128, layerMask))
+            {
+                // play sound queue
+
+                yield return new WaitForSeconds(aimTime);
+
+                playerVector = player.transform.position - transform.position;
+                Vector3 laserVector = hit.transform.position - transform.position;
+
+                yield return new WaitForSeconds(0.4f);
+
+                // Deploy laser
+                GameObject laser = Instantiate(laserPrefab, laserVector / 2 + transform.position,
+                            Quaternion.LookRotation(laserVector, Vector3.up));
+                laser.transform.Rotate(Vector3.right, 90, Space.Self);
+                laser.transform.localScale = new Vector3(1, laserVector.magnitude / 2, 1);
+
+                yield return new WaitForSeconds(laserTime);
+
+                Destroy(laser);
+            }*/
+
+
+            // play sound queue
+
+            Vector3 playerVector = player.transform.position - transform.position;
+
+            if (Physics.Raycast(transform.position, playerVector, out RaycastHit hit, 256, layerMask))
+            {
+                Debug.DrawRay(transform.position, hit.transform.position);
             }
         }
     }
